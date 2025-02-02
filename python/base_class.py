1d1959643587be66aafc0eaf4cc5f2d1ad903041
Zero Day <zerodayknowledge@gmail.com>
2025-02-02 07:52:51 -0700

    added more coding lessons

--- python/base_class.py
+++ python/base_class.py
@@ -7,18 +7,58 @@ class base_class:
         pass
 
     def flow_controller(self):
-        # self.lesson_1()
-        self.lesson_2()
+        ### Comment and uncomment the following lines to execute the examples
+        self.hello_world()
+        self.if_example()
+        self.if_else_ample()
+        self.if_elif_example()
+        self.for_loop_with_integers_example()
+        self.for_loop_with_strings_example()
+        self.while_do_loop_example()
 
-    def lesson_1(self):
-        print("Hello World")
+    def hello_world(self):
+        print("Hello Python World")
 
-    def lesson_2(self):
+    def if_example(self):
+        #play with the value of variable "somevalue" to see the other code path execute
         somevalue=35
         if(somevalue<40):
-            print("Fuck You")
+            print("Fuck You 1")
+
+    def if_else_ample(self):
+        #play with the value of variable "somevalue" to see the other code path execute
+        somevalue=35
+        if(somevalue<40):
+            print("Fuck You 2")
         else:
-            print("Fuck You Again")
+            print("Fuck You Again 3")
+
+    def if_elif_example(self):
+        #play with the value of variable "somevalue" to see the other code path execute
+        somevalue=70
+        if(somevalue<69):
+            print("Fuck You 5")
+        elif(somevalue>69):
+            print("Fuck You Again 6")
+
+    def for_loop_with_integers_example(self):
+        #print a santa greating
+        for x in range(0,3):
+            print("Ho!")
+
+    def for_loop_with_strings_example(self):
+        #print a bad santa greating
+        santa_talk =["Santa","likes","his","hoes!"]
+        for x in santa_talk:
+            print(x)
+
+    def while_do_loop_example(self):
+        #print a bad santa greating
+        x = 0
+        while x<3:
+            print("Hoe!")
+            x+=1
+
 
 def main():
     controller = base_class()
